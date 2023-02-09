f6c58188c68b0b88529e1b10a6e3066943089736
Patrick Rhodes Martin <patrick.martin.r@gmail.com>
2023-02-09 13:15:24 -0800

    Make updates to package to support SABRE.js

--- README.md
+++ README.md
@@ -2,66 +2,29 @@
 
 Add **Advanced SubStation Alpha (ASS)** subtitles support to
 [videojs](https://github.com/videojs/video.js) using the
-[libjass](https://github.com/Arnavion/libjass) library.
-
-Check out the demo
-[here](https://sunnyli.github.io/videojs-ass/example.html)
-
+[SABRE.js](https://github.com/SABRE-JS/SABRE.js) library.
 
 ## Install
 
-For plugin that supports videojs v5.x install using either:
-
-- `bower install videojs-ass`
-
-or
+For plugin:
 
 - `npm install videojs-ass`
-
-For videojs v4:
-
-Just specify version to be within:
-
-```
-"videojs-ass": ">=0.3.0 < 0.5.0"
-```
-for bower or npm whichever you prefer using.
 
 
 ## Usage
 
-Initialize the `ass` plugin with the `src` field like the following:
+Initialize the `ass_sabre` plugin with and provide a subtitle track of the file along with a metadata text track with the label `fonts` which contains a JSON array of urls of fonts to load, one of these must be the font `Arial`:
 
 ```
 videojs('player_id', {
   plugins: {
-    ass: {
-      src: 'subs/subtitles.ass'
+    ass_sabre: {
+      renderMode: 'bitmap'
     }
   }
 }
 ```
 
-| Option      | Default       | Description                                                |
-| ----------- | ------------- | ---------------------------------------------------------- |
-| src         | -<sup>1</sup> | `.ass` / `.ssa` source.                                    |
-| label       | -<sup>2</sup> | subtitle track label that shows up in the subtitles picker |
-| delay       | 0<sup>3</sup> | delay subtitle rendering by the specified value in seconds |
-| rate        | 1             | subtitle update speed relative to video playback rate      |
-| enableSvg   | true          | see [here][svg-effects] regarding SVG filter               |
-| fontMap     | -             | see [here][font-map] regarding using custom web fonts      |
-| fontMapById | -             | alternate to above, takes id and runs [this][font-map-el]  |
-| videoWidth  | -<sup>3</sup> | metadata to assist in determining the optimal (cont below) |
-| videoHeight | -<sup>3</sup> | (cont) subtitle letterboxing ratio                         |
-
-**Footnotes:**
-
-1. This property is required!
-2. Has fallback values but you should provide a better label.
-3. Value can be negative
-4. Generally, you should set these values when using external videojs providers
-   as they might not expose the video dimensions to the player.
-
-[svg-effects]: https://github.com/Arnavion/libjass/blob/v0.10.0/README.md#what-browser-and-javascript-features-does-libjass-need
-[font-map]: https://arnavion.github.io/libjass/api.xhtml#libjass.renderers.RendererSettings.fontMap
-[font-map-el]: https://arnavion.github.io/libjass/api.xhtml#libjass.renderers.RendererSettings.makeFontMapFromStyleElement
+| Option      | Default       | Description                                                                                                    |
+| ----------- | ------------- | -------------------------------------------------------------------------------------------------------------- |
+| renderMode  | `bitmap`      | This may either be set to `2d` or `bitmap`, `bitmap` may be faster, but `2d` is compatable with more browsers. |
---
+++ src/videojs.ass_sabre.js
@@ -0,0 +1,207 @@
+/*! videojs-ass-sabre
+ * Copyright (c) 2023 Patrick Rhodes Martin
+ * based on videojs-ass by Sunny Li
+ * Licensed under the Apache-2.0 license. */
+(function (videojs, sabre, opentype) {
+  'use strict';
+
+  const Plugin = videojs.getPlugin('plugin');
+  class ASSSABREPlugin extends Plugin {
+    VERSION = '0.0.1';
+    overlay = null;
+    renderers = [];
+    cur_id = -1;
+    loop = null;
+    loopHandle = null;
+    constructor(player, options) {
+      super(player, options);
+      let _this = this;
+      let renderer_callbacks = [];
+      let fonts = [];
+      let new_fonts = true;
+      let fonts_ready = false;
+      let OverlayComponent = null;
+      let tracks = player.textTracks();
+      let renderMethod = 'bitmap';
+
+      this.overlay = document.createElement('canvas');
+
+      this.on(player, ['playing', 'pause'], this.updateState);
+
+      if (typeof options.renderMethod === 'string' && options.renderMethod === '2d') {
+        renderMethod = options.renderMethod;
+      }
+
+      let overlayContainer = document.createElement('div');
+      overlayContainer.className = 'vjs-ass-sabre';
+      overlayContainer.appendChild(this.overlay);
+      OverlayComponent = {
+        name: function () {
+          return 'AssOverlay';
+        },
+        el: function () {
+          return overlayContainer;
+        },
+      };
+
+      player.addChild(OverlayComponent, {}, 3);
+
+      function parseFont(fontData) {
+        return opentype.parse(fontData);
+      }
+
+      function loadArrayBuffer(uri, callback) {
+        let request = new XMLHttpRequest();
+        request.responseType = 'arraybuffer';
+        request.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            this.onreadystatechange = null;
+            callback(this.response);
+          }
+        };
+        request.open('GET', uri, true);
+        request.send(null);
+      }
+
+      function fetchFonts(fontsListURI) {
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            this.onreadystatechange = null;
+            let fontsList = JSON.parse(this.responseText);
+            let fontCounter = 0;
+            for (let idx = 0; idx < fontsList.length; idx++) {
+              let fontURI = fontsList[idx];
+              loadArrayBuffer(fontURI, function (buff) {
+                fonts.push(opentype.parse(buff));
+                if (++fontCounter == fontsList.length) {
+                  fonts_ready = true;
+                  let subs_callbacks = renderer_callbacks;
+                  renderer_callbacks = [];
+                  for (let i = 0; i < subs_callbacks.length; i++) {
+                    let callback_pair = subs_callbacks[i];
+                    callback_pair.callback.call(null, callback_pair.subs, callback_pair.id);
+                  }
+                }
+              });
+            }
+          }
+        };
+        request.open('GET', fontsListURI, true);
+        request.send(null);
+      }
+
+      function fetchSubtitleFile(uri, callback, id) {
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            this.onreadystatechange = null;
+            if (fonts_ready) {
+              callback(this.responseText, id);
+            } else {
+              renderer_callbacks.push({ callback: callback, id: id, subs: this.responseText });
+            }
+          }
+        };
+        request.open('GET', uri, true);
+        request.send(null);
+      }
+
+      function renderLoop() {
+        _this.loopHandle = window.requestAnimationFrame(renderLoop);
+        if (_this.renderers[_this.cur_id] && _this.renderers[_this.cur_id].checkReadyToRender()) {
+          _this.renderers[_this.cur_id].drawFrame(player.currentTime(), _this.overlay, renderMethod);
+        }
+      }
+
+      this.loop = renderLoop;
+
+      player.on('sourceset', function () {
+        fonts = [];
+        new_fonts = true;
+        fonts_ready = false;
+      });
+      this.displayUpdate = this.updateDisplayArea.bind(this);
+      window.addEventListener('resize', this.displayUpdate);
+      player.on('loadedmetadata', this.displayUpdate);
+      player.on('resize', this.displayUpdate);
+      player.on('fullscreenchange', this.displayUpdate);
+
+      function updateSubtitleSelections() {
+        let SSA_counter = 0;
+        let active_track = false;
+        _this.cur_id = -1;
+        if (new_fonts) {
+          for (let index = 0; index < tracks.length; index++) {
+            let track = tracks[index];
+            let src = tracks.tracks_[index].src;
+            if (track.kind == 'metadata' && track.label == 'fonts') {
+              fetchFonts(src);
+              new_fonts = false;
+              break;
+            }
+          }
+        }
+        for (let index = 0; index < tracks.length; index++) {
+          let track = tracks[index];
+          let src = tracks.tracks_[index].src;
+
+          if (src && (src.endsWith('.ass') || src.endsWith('.ssa'))) {
+            if (!_this.renderers[SSA_counter]) {
+              _this.renderers[SSA_counter] = sabre.SABRERenderer(parseFont);
+              fetchSubtitleFile(
+                src,
+                function (subs, id) {
+                  _this.renderers[id].loadSubtitles(subs, fonts);
+                },
+                SSA_counter,
+              );
+            }
+
+            if (track.kind === 'subtitles' && track.mode === 'showing') {
+              _this.cur_id = SSA_counter;
+              active_track = true;
+            }
+
+            SSA_counter++;
+          }
+        }
+
+        if (active_track) {
+          overlayContainer.style.display = '';
+          _this.updateState();
+        } else {
+          overlayContainer.style.display = 'none';
+        }
+      }
+      updateSubtitleSelections();
+      tracks.on('change', updateSubtitleSelections);
+    }
+
+    updateDisplayArea() {
+      // player might not have information on video dimensions when using external providers
+      let videoWidth = this.player.videoWidth() || this.player.el().offsetWidth;
+      let videoHeight = this.player.videoHeight() || this.player.el().offsetHeight;
+      this.display.width = videoWidth;
+      this.display.height = videoHeight;
+      if (this.cur_id >= 0) this.renderers[this.cur_id].setViewport(videoWidth, videoHeight);
+    }
+
+    dispose() {
+      super.dispose();
+      window.removeEventListener('resize', this.displayUpdate);
+    }
+
+    updateState() {
+      if (this.cur_id >= 0) {
+        if (this.player.paused()) {
+          if(this.loopHandle !== null) window.cancelAnimationFrame(this.loopHandle);
+          this.loopHandle = null;
+        } else {
+          this.loopHandle = window.requestAnimationFrame(this.loop);
+        }
+      }
+    }
+  }
+  videojs.registerPlugin('ass_sabre', ASSSABREPlugin);
+})(window.videojs, window.sabre, window.opentype);
